a2728b4c8a2d5c8c0acf152a55ecf4a88cda2e8b
quanquanquanquanquan <43127957+quanquanquanquanquan@users.noreply.github.com>
2020-07-02 03:17:41 +0800

    Add files via upload

---
+++ Application/Admin/Controller/UserController.class.php
@@ -0,0 +1,64 @@
+<?php
+namespace Admin\Controller;
+//前台会员控制器
+class UserController extends CommonController{
+	public function index(){
+		//取出会员信息
+		$data = D('User')->getList();
+		$data['priv'] = session('userinfo.priv');
+		$this->assign($data);
+		$this->display();
+	}
+	//会员修改
+	public function edit(){
+	   
+	    $id = I('get.id/d',0);      
+	   
+	    $User = D('User');
+	    
+	    if(IS_POST){
+	       
+			if(!$User->create(null,2)){
+				$this->error('修改失败：'.$User->getError());
+			}
+			//保存到数据库
+			if(false === $User->where(array('id'=>$id))->save()){
+				$this->error('修改失败：保存到数据库失败。');
+			}
+			//保存成功
+			$this->redirect('User/index');
+	    }
+	    //准备where条件
+	    $where = array('id'=>$id);
+	    //查询产品数据
+	    $data['user'] = $User->getUser($where);
+	    if(!$data['user']){
+	        $this->error('用户不存在！');
+	    }
+	    
+	    $data['priv'] = session('userinfo.priv');
+	    $this->assign($data);
+	    $this->display();
+	}
+	//删除
+	public function del(){
+	    
+	    if(!IS_POST) $this->error('删除失败！');
+	  
+	    $id = I('post.id/d',0);  //ID
+	    
+	    $jump = U('User/index');
+	    
+	    $User = D('User');
+	    
+	    if(!$User->autoCheckToken($_POST)){
+	        $this->error('表单已过期，请重新提交',$jump);
+	    }
+	    
+	    $where = array('id'=>$id);
+	    
+	    $User->where($where)->delete();
+	    
+	    redirect($jump);
+	}
+}
