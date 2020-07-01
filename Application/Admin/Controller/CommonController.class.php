7b4489a82c42ba2d2534462060fbac7478ee9cdf
quanquanquanquanquan <43127957+quanquanquanquanquan@users.noreply.github.com>
2020-07-01 20:03:38 +0800

    Create CommonController.class.php

---
+++ Application/Admin/Controller/CommonController.class.php
@@ -0,0 +1,24 @@
+<?php
+namespace Admin\Controller;
+use Think\Controller;
+//后台公共控制器
+class CommonController extends Controller{
+	 
+    public function __construct() {
+        parent::__construct();  
+		$this->checkUser();    
+		 
+		$this->assign('admin_name',session('userinfo.name'));
+		$this->assign('admin_priv',session('userinfo.priv'));
+    }
+	//检查用户是否已经登录
+	private function checkUser(){
+		if(!session('?userinfo')){
+			 
+			$this->redirect('Login/index');
+		}
+	}
+	public function _empty($name){
+		$this->error('无效的操作：'.$name);
+    }
+}
